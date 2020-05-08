974755de8fc8a1ef69b351089bc786024cc21850
ngua <rory.hayford@gmail.com>
2020-05-08 16:28:39 +0700

    Fixed entrypoint.prod

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for postgres..."
-
-    while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 1
-    done
-
-    echo "Postgres started"
-fi
-
-exec "$@"
-
 python manage.py collectstatic --no-input --clear --settings=$DJANGO_SETTINGS_MODULE
 python manage.py migrate --no-input --settings=$DJANGO_SETTINGS_MODULE
 
