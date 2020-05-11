30dede0e739d6f48fe8748ee16aedd14cad4e484
ngua <rory.hayford@gmail.com>
2020-05-11 16:13:32 +0700

    More SSL

--- settings/prod.py
+++ settings/prod.py
@@ -5,8 +5,6 @@ from .base import *
 
 DEBUG = False
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
-SECURE_SSL_REDIRECT = True
-SECURE_REFERRER_POLICY = 'same-origin'
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
