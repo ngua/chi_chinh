1b6c4cb8e0bc28f751a4aba4934c71cf18a378f9
ngua <rory.hayford@gmail.com>
2020-05-11 18:16:17 +0700

    Moved cache to template to avoid invalidating setlang csrf token

--- settings/prod.py
+++ settings/prod.py
@@ -6,6 +6,7 @@ from .base import *
 DEBUG = False
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
 SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 INSTALLED_APPS = [
     'jet',
