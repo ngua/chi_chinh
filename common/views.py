0cc447f7ecd8b0b21291a1c467b8189795190dff
ngua <rory.hayford@gmail.com>
2020-05-11 17:52:57 +0700

    csrf in setlang buttons

--- common/views.py
+++ common/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.decorators.cache import cache_page
 from django.core.cache.backends.base import DEFAULT_TIMEOUT
-from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
 from recipes.models import Recipe
 
@@ -10,7 +10,7 @@ CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 
 
 @cache_page(CACHE_TTL)
-@csrf_protect
+@csrf_exempt
 def index(request):
     latest = Recipe.objects.order_by('-created')[:5]
     featured_recipes = Recipe.objects.filter(featured=True)
